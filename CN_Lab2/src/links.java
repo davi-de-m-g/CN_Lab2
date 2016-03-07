aa69704cc935b8a072f4c2297cdec4e5f2c41961
dendavi <dvd_15150@hotmail.com>
2016-03-07 16:00:39 +0100

    New sheiss

---
+++ CN_Lab2/src/links.java
@@ -0,0 +1,17 @@
+
+public class links {
+	
+	Exception in thread "main" java.lang.NullPointerExceptiona
+	http://stackoverflow.com/questions/26419065/cannot-convert-type-string-to-byte
+		
+	http://www.coderanch.com/t/465767/java/java/getHardwareAddress-returning-null
+	
+	https://github.com/jaromil/rogued
+	http://www.codeproject.com/Questions/828017/How-do-run-a-DHCP-java-code
+	
+	https://android.googlesource.com/platform/frameworks/base/+/777f5b2/core/java/android/net/dhcp/DhcpPacket.java
+	
+	https://www.google.be/search?client=ubuntu&channel=fs&q=build+dhcp+message&ie=utf-8&oe=utf-8&gfe_rd=cr&ei=Z4bdVsOgCIH5-AaxtrRQ#q=build+dhcp+message+java&channel=fs&tbs=qdr:y
+	
+			
+}
